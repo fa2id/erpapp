53dbdd704ba9c6462fb94bc83950f35f1b3c5f14
fa2id <faridsenior@gmail.com>
2018-10-08 17:47:13 -0400

    fix conflicts

--- src/main/java/com/fa2id/erpapp/repository/RoleRepository.java
+++ src/main/java/com/fa2id/erpapp/repository/RoleRepository.java
@@ -1,4 +1,11 @@
 package com.fa2id.erpapp.repository;
 
-public class RoleRepository {
+import com.fa2id.erpapp.model.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Integer> {
+
+    Role findByRole(String role);
 }
